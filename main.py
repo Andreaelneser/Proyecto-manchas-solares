d53564825f4b15a2be6c9457b4a1afbd610cbf17
Andreaelneser <69821359+Andreaelneser@users.noreply.github.com>
2020-11-02 19:41:59 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -14,18 +14,18 @@ def plotImg(img):
 path = 'D:/Documents/Universidad/Lala/Hola/PROYECTO/Sol'
 path_file = os.path.join(path, 'Sol*.jpg')
 images = glob.glob(path_file)
-#img = cv2.imread('sol1.jpg')
+
 for img_name in images:
     img = cv2.imread(img_name)
     gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     binary_img = cv2.adaptiveThreshold(gray_img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY_INV, 131, 15)
-    plotImg(binary_img)
+    plotImg(binary_img) #imagen en threshold
     _, _, boxes, _ = cv2.connectedComponentsWithStats(binary_img)
 
     boxes = boxes[1:]
     filtered_boxes = []
     for x, y, w, h, pixels in boxes:
-        if pixels > 10 and h < 200 and w < 200 and h > 0 and w > 0: #tamaño de manchas
+        if pixels > 3 and h < 200 and w < 200 and h > 0 and w > 0: #tamaño de manchas
             filtered_boxes.append((x, y, w, h))
 
     for x, y, w, h in filtered_boxes:
